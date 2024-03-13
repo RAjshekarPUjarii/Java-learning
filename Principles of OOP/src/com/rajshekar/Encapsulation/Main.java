edf2ca7826ea3be241d3733e27c94d9932c35ab2
Rajshekar pujari <130205130+RAjshekarPUjarii@users.noreply.github.com>
2024-03-13 20:41:47 +0530

    Update Main.java

--- Principles of OOP/src/com/rajshekar/Encapsulation/Main.java
+++ Principles of OOP/src/com/rajshekar/Encapsulation/Main.java
@@ -7,14 +7,13 @@ public class Main {
 
 //    getter function access a private data member as show below
 
-//        System.out.println(obj.getName());
-//        System.out.println(obj.getNum());
-//      setter function  to set a private data member as shown below
-
-//        obj.setNum(3);
-//        obj.setName("shivling");
-//        System.out.println(obj.getName());
-//        System.out.println(obj.getNum());
+      System.out.println(obj.getName());
+        System.out.println(obj.getNum());   
+        //setter function  to set a private data member as shown below
+      obj.setNum(3); 
+        obj.setName("shivling");
+     System.out.println(obj.getName());
+    System.out.println(obj.getNum());
 
         System.out.println(obj.num);
 
